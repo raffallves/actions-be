bef15c0ad2d7e3e1ab0c6d3e797efc33c63088c7
raffallves <89459281+raffallves@users.noreply.github.com>
2024-11-05 21:37:49 -0300

    tesss

--- lambda1/index.mjs
+++ lambda1/index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
     const response = {
       statusCode: 200,
-      body: JSON.stringify('L1V2'),
+      body: JSON.stringify('L1V3'),
     };
     return response;
   };
