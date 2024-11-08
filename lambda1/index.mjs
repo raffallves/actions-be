af6ecf033a9e497ef8e0d1180c2c32265c3285d9
raffallves <89459281+raffallves@users.noreply.github.com>
2024-11-07 21:51:29 -0300

    hmm

--- lambda1/index.mjs
+++ lambda1/index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
     const response = {
       statusCode: 200,
-      body: JSON.stringify('L1V3'),
+      body: JSON.stringify('L1V7'),
     };
     return response;
   };
