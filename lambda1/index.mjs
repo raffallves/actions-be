6145709fc661d4438488600b1622a27c39313e21
raffallves <89459281+raffallves@users.noreply.github.com>
2024-11-07 22:29:03 -0300

    vai caralho

--- lambda1/index.mjs
+++ lambda1/index.mjs
@@ -1,7 +1,7 @@
 export const handler = async (event) => {
     const response = {
       statusCode: 200,
-      body: JSON.stringify('L1V8'),
+      body: JSON.stringify('L1V9'),
     };
     return response;
   };
